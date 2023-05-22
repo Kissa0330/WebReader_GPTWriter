f7ceec8c6ad9a245f3b12ec4440ab9aebbc0029b
らいか <raika4789@gmail.com>
2023-05-23 05:43:56 +0900

    一旦完成

--- main.js
+++ main.js
@@ -1,41 +1,63 @@
 import MyOpenAIApi from "./openai.js";
-import fs from "fs"
+import fs from "fs";
 import { getHTML, extractTextFromHTML } from "./treatHTML.js";
 
-async function urlToFeature(url, object, name){
-	let html = await getHTML(url);
-	let text = extractTextFromHTML(html);
-	const content = `# 命令\n以下のテキストは${object}の${name}の情報が掲載されているサイトのテキストです。${name}の特徴を教えてください。\n# テキスト\n${text}`
-	const response = await MyOpenAIApi.createChatCompletion({model: "gpt-3.5-turbo", messages: [{role:"user", content: content}]});
-	return response.data.choices[0].message.content;
+async function urlToFeature(url, object, name) {
+  let html = await getHTML(url);
+  let text = extractTextFromHTML(html);
+  console.log("情報の文字数:" + text.length);
+  if (text.length > 3000)
+  {
+	return "情報が多すぎて、特徴が駐質できませんでした。"
+  }
+  const content = `# 命令\n以下のテキストは${object}の${name}の情報が掲載されているサイトのテキストです。${name}の特徴を教えてください。\n# テキスト\n${text}`;
+  const response = await MyOpenAIApi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: content }],
+  });
+  return response.data.choices[0].message.content;
 }
 
-async function featureToText(feature, object, name, number, length){
-	const content = `# 命令\nあなたはプロのWebライターです。${object}の情報を渡すので、紹介記事を${length}~${length+100}文字で執筆してください。\nフォーマットに従って番号とはちみつ専門店の名前を出力してください。\n情報が少なく執筆が困難な場合は、フォーマットと「情報が少なく執筆が困難です」という文章のみを出力して下さい。\n# フォーマット\n番号.${object}の名前\n# ${object}の情報\n## 名前\n${name}\n## 番号\n${number}\n## 特徴\n${feature}`
-	let response;
-	for (let i = 0; i < 3; i++)
-	{
-		response = await MyOpenAIApi.createChatCompletion({model: "gpt-4", messages: [{role:"user", content: content}]});
-		if (response.data.choices[0].message.content.length >= length)
-		{
-			break ;
-		}
-	}
-	return response.data.choices[0].message.content;
+async function featureToText(feature, object, name, number, length) {
+  const content = `# 命令\nあなたはプロのWebライターです。${object}の情報を渡すので、紹介記事を${length}~${
+    length + 100
+  }文字で執筆してください。\nフォーマットに従って番号とはちみつ専門店の名前を出力してください。\n情報が少なく執筆が困難な場合は、フォーマットと「情報が少なく執筆が困難です」という文章のみを出力して下さい。\n# フォーマット\n番号.${object}の名前\n# ${object}の情報\n## 名前\n${name}\n## 番号\n${number}\n## 特徴\n${feature}`;
+  const response = await MyOpenAIApi.createChatCompletion({
+    model: "gpt-4",
+    messages: [{ role: "user", content: content }],
+  });
+  return response.data.choices[0].message.content;
 }
 
-async function writeArticle(url, object, number, length, name)
-{
-	const feature = await urlToFeature(url, object, name)
-	const text = await featureToText(feature, object, name, number, length);
-	
-	const content = text + "\n文字数: " + text.length + "文字";
-	
-	const writeStream = fs.createWriteStream('./article.txt');
-	writeStream.write(content, 'utf8');
-	writeStream.end();
-	
-	writeStream.on('error', (err) => {
-	  console.error(err);
+async function writeArticle(url, object, number, length, name) {
+  const feature = await urlToFeature(url, object, name);
+  const text = await featureToText(feature, object, name, number, length);
+
+  const content =
+    text + "\n\n";
+	console.log("記事\n" + content);
+	fs.appendFile('./article.txt', content, (err) => {
+		if (err) throw err;
 	});
 }
+
+const path = process.argv.slice(2)[0];
+fs.readFile(path, "utf8", async function (err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const lines = data.split("\n");
+  const columns = lines[0].split(",");
+  const object = columns[0];
+  const length = parseInt(columns[1]);
+  for (let i = 1; i < lines.length; i++) {
+    let url = lines[i].split(",")[0];
+    let number = lines[i].split(",")[1];
+    let name = lines[i].split(",")[2];
+	console.log("now target is: " + number + "." + name)
+    await writeArticle(url, object, number, length, name);
+	console.log("--------------------")
+  }
+});
